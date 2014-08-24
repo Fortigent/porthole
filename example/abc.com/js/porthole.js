e57c16ccab6dbbe01a6956b612363a02374ac333
abhifortigent <abhishek.sharma@fortigent.com>
2014-08-24 14:02:45 -0400

    updated ie8 to use native window.postMessage support. fixed abc.com and xyz.com example to work with iis virtual dir path.

--- example/abc.com/js/porthole.js
+++ example/abc.com/js/porthole.js
@@ -300,42 +300,54 @@ iFrame proxy abc.com->abc.com: forwardMessageEvent(event)
      * Implementation for modern browsers that supports it
      */
     Porthole.WindowProxyHTML5 = Porthole.WindowProxyBase.extend({
-        init: function(proxyIFrameUrl, targetWindowName) {
-            this._super(targetWindowName);
-            this.eventListenerCallback = null;
-        },
+    	init: function (proxyIFrameUrl, targetWindowName) {
+    		this._super(targetWindowName);
+    		this.eventListenerCallback = null;
+    	},
 
-        dispatchMessage: function(message) {
-            this.getTargetWindow().postMessage(Porthole.WindowProxy.serialize(message), message.targetOrigin);
-        },
+    	dispatchMessage: function (message) {
+    		this.getTargetWindow().postMessage(Porthole.WindowProxy.serialize(message), message.targetOrigin);
+    	},
 
-        addEventListener: function(f) {
-            if (this.eventListeners.length === 0) {
-                var self = this;
-                this.eventListenerCallback = function(event) { self.eventListener(self, event); };
-                window.addEventListener('message', this.eventListenerCallback, false);
-            }
-            return this._super(f);
-        },
+    	addEventListener: function (f) {
+    		if (this.eventListeners.length === 0) {
+    			var self = this;
+    			if (window.addEventListener) {
+    				this.eventListenerCallback = function (event) { self.eventListener(self, event); };
+    				window.addEventListener('message', this.eventListenerCallback, false);
+    			} else if (window.attachEvent) {
+    				// Make IE8 happy, just not that 1. postMessage only works for IFRAMES/FRAMES http://blogs.msdn.com/b/ieinternals/archive/2009/09/16/bugs-in-ie8-support-for-html5-postmessage-sessionstorage-and-localstorage.aspx
+    				this.eventListenerCallback = function (event) { self.eventListener(self, window.event); };
+    				window.attachEvent("onmessage", this.eventListenerCallback);
+    			}
+    		}
+    		return this._super(f);
+    	},
 
-        removeEventListener: function(f) {
-            this._super(f);
+    	removeEventListener: function (f) {
+    		this._super(f);
 
-            if (this.eventListeners.length === 0) {
-                window.removeEventListener('message', this.eventListenerCallback);
-                this.eventListenerCallback = null;
-            }
-        },
+    		if (this.eventListeners.length === 0) {
+    			if (window.removeEventListener) {
+    				window.removeEventListener('message', this.eventListenerCallback);
+    			} else if (window.detachEvent) { // Make IE8, happy, see above
+    				// see jquery, detachEvent needed property on element, by name of that event, to properly expose it to GC
+    				if (typeof window.onmessage === 'undefined') window.onmessage = null;
+    				window.detachEvent('onmessage', this.eventListenerCallback);
+    			}
+    			this.eventListenerCallback = null;
+    		}
+    	},
 
-        eventListener: function(self, nativeEvent) {
-            var data = Porthole.WindowProxy.unserialize(nativeEvent.data);
-            if (data && (self.targetWindowName == '' || data.sourceWindowName == self.targetWindowName)) {
-                self.dispatchEvent(new Porthole.MessageEvent(data.data, nativeEvent.origin, self));
-            }
-        }
+    	eventListener: function (self, nativeEvent) {
+    		var data = Porthole.WindowProxy.unserialize(nativeEvent.data);
+    		if (data && (self.targetWindowName === '' || data.sourceWindowName == self.targetWindowName)) {
+    			self.dispatchEvent(new Porthole.MessageEvent(data.data, nativeEvent.origin, self));
+    		}
+    	}
     });
 
-    if (typeof window.postMessage !== 'function') {
+    if (!window.postMessage) {
         Porthole.trace('Using legacy browser support');
         Porthole.WindowProxy = Porthole.WindowProxyLegacy.extend({});
     } else {
